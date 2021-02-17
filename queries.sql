a41e1f2c2eb86e0d72f1be6d61bd66968ef0e1c8
Jennifer Kramer <jenkrame@gmail.com>
2021-02-17 17:01:28 -0500

    Completed queries in queries.sql file.

--- queries.sql
+++ queries.sql
@@ -12,17 +12,24 @@ JOIN Category AS c
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 SELECT 
-    o.id OrderID,
-    s.companyname CompanyName,
-    orderdate OrderDate
+    o.Id OrderID,
+    CompanyName,
+    OrderDate
 FROM `Order` AS o
 JOIN `Shipper` AS s
     ON o.shipvia = s.id
-WHERE orderdate < '2012-08-09'
+WHERE OrderDate < '2012-08-09'
 
--- Display the (ProductName)name and (QuantityPerUnit)quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+-- Display the (ProductName)name and (Quantity)quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
-
+SELECT
+    OrderId,
+    ProductName,
+    Quantity
+FROM `Product` AS p
+JOIN `OrderDetail` AS od
+    ON p.Id = od.ProductId
+WHERE od.OrderId = '10251'
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
