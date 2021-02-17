492484872fb2ccde226ab39fc5176ccffad71411
Jennifer Kramer <jenkrame@gmail.com>
2021-02-17 16:20:33 -0500

    Got last query done. Working on 3rd query in queries.sql file.

--- queries.sql
+++ queries.sql
@@ -20,8 +20,18 @@ JOIN `Shipper` AS s
     ON o.shipvia = s.id
 WHERE orderdate < '2012-08-09'
 
--- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+-- Display the (ProductName)name and (QuantityPerUnit)quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT
+    o.Id OrderID,
+    c.CompanyName,
+    e.LastName
+FROM `Order` AS o
+JOIN `Customer` AS c
+    ON o.CustomerId = c.Id
+JOIN `Employee` AS e
+    ON o.EmployeeId = e.Id
