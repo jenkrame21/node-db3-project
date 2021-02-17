d2081347ae8271e1c355d86d2450031a2495071a
Jennifer Kramer <jenkrame@gmail.com>
2021-02-17 17:33:53 -0500

    Started filling out scheme-model.js file with helpers listed on ReadMe file.

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,29 @@
 const db = require("../../data/db-config.js");
+
+module.exports = {
+
+    find(id) {
+        return db("schemes as sch")
+    },
+
+    findById(id) {
+        
+    },
+
+    findSteps(id) {
+
+    },
+
+    add(scheme) {
+
+    },
+
+    update(changes, id) {
+
+    },
+
+    remove(id) {
+
+    }
+
+};
