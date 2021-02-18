f8fcde272837b5abb14ee0f12679315c8e8a3d82
Jennifer Kramer <jenkrame@gmail.com>
2021-02-18 10:19:58 -0500

    Able to get findSteps on 3rd GET request working and errors too! Onto POST...

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -2,16 +2,11 @@ const db = require("../../data/db-config.js");
 
 module.exports = {
 
-    find() {
-        return db("schemes")
-    },
-
-    findById(id) {
-
-    },
-
     findSteps(id) {
-
+        return db("schemes as sch")
+            .join("steps as stp", "stp.scheme_id", "sch.id")
+            .select("sch.scheme_name as Scheme", "stp.scheme_id as SchemeId", "stp.step_number as StepNumber", "stp.instructions as Instructions")
+            .where("sch.id", id);
     },
 
     add(scheme) {
